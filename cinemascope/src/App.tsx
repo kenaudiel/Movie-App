91fe8bd92d8e75f8629c841a37e31ef689541a9e
Kenneth <audielkenneth23@gmail.com>
2026-05-11 17:35:05 +0700

    add movie list section

--- cinemascope/src/App.tsx
+++ cinemascope/src/App.tsx
@@ -7,17 +7,20 @@ export default function App(){
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
             <Navbar/>
 
-            <div className="grid grid-cols-4 gap-y-5">
-                <CardImage title="Card #1" description="This is the placeholder for the card description." badge="Free" buttonText="Learn More"/>
-                <CardImage title="Card #2" description="This is the placeholder for the card description." badge="Free" buttonText="Learn More"/>
-                <CardImage title="Card #3" description="This is the placeholder for the card description." badge="Free" buttonText="Learn More"/>
-                <CardImage title="Card #4" description="This is the placeholder for the card description." badge="Free" buttonText="Learn More"/>
-                <CardImage title="Card #5" description="This is the placeholder for the card description." badge="Free" buttonText="Learn More"/>
-                <CardImage title="Card #6" description="This is the placeholder for the card description." badge="Free" buttonText="Learn More"/>
-                <CardImage title="Card #7" description="This is the placeholder for the card description." badge="Free" buttonText="Learn More"/>
-                <CardImage title="Card #8" description="This is the placeholder for the card description." badge="Free" buttonText="Learn More"/>
-            </div>
-            
+            <section className="p-10">
+                <h2 className="mb-5">Popular Movies</h2>   
+                <div className="grid grid-cols-4 gap-5">
+                    <CardImage title="Card #1" description="This is the placeholder for the card description." badge="Free" buttonText="Learn More"/>
+                    <CardImage title="Card #2" description="This is the placeholder for the card description." badge="Free" buttonText="Learn More"/>
+                    <CardImage title="Card #3" description="This is the placeholder for the card description." badge="Free" buttonText="Learn More"/>
+                    <CardImage title="Card #4" description="This is the placeholder for the card description." badge="Free" buttonText="Learn More"/>
+                    <CardImage title="Card #5" description="This is the placeholder for the card description." badge="Free" buttonText="Learn More"/>
+                    <CardImage title="Card #6" description="This is the placeholder for the card description." badge="Free" buttonText="Learn More"/>
+                    <CardImage title="Card #7" description="This is the placeholder for the card description." badge="Free" buttonText="Learn More"/>
+                    <CardImage title="Card #8" description="This is the placeholder for the card description." badge="Free" buttonText="Learn More"/>
+                </div>
+            </section>
+
         </ThemeProvider>
     );
 }
