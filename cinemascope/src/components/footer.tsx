b6fac3aeb123d237ab91cd5cb3136f3f803c9df4
Kenneth <audielkenneth23@gmail.com>
2026-05-20 22:28:37 +0700

    add footer links list

--- cinemascope/src/components/footer.tsx
+++ cinemascope/src/components/footer.tsx
@@ -1,14 +1,21 @@
 import { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuLink } from "./ui/navigation-menu";
 import { FaFacebook, FaInstagram, FaTiktok, FaYoutube } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
-type socialMediaProps = {
+type socialMediaType = {
     key:number,
     icon:React.ReactNode,
     href:string,
 }
 
-const socialMedia:socialMediaProps[] = [
+type footerLinksType = {
+    key:number,
+    text:string,
+    path:string
+}
+
+const socialMedia:socialMediaType[] = [
     {
         key:1,
         icon:<FaFacebook />,
@@ -36,9 +43,56 @@ const socialMedia:socialMediaProps[] = [
     },
 ]
 
+const footerLinks: footerLinksType[] = [
+    {
+        key:1,
+        text:"Terms",
+        path:""
+    },
+    {
+        key:2,
+        text:"Privacy",
+        path:""
+    },
+    {
+        key:3,
+        text:"Security",
+        path:""
+    },
+    {
+        key:4,
+        text:"Community",
+        path:""
+    },
+    {
+        key:5,
+        text:"Docs",
+        path:""
+    },
+    {
+        key:6,
+        text:"Contact",
+        path:""
+    },
+    {
+        key:7,
+        text:"Help",
+        path:""
+    },
+    {
+        key:8,
+        text:"Manage Cookies",
+        path:""
+    },
+]
+
 export default function Footer(){
     return(
-        <NavigationMenu className="flex border-t-2 items-center justify-center p-10">
+        <NavigationMenu className="flex border-t-2 items-center justify-center p-10 gap-30">
+            <NavigationMenuList className="flex gap-5">
+                {footerLinksList()}
+            </NavigationMenuList>
+
             <NavigationMenuList className="flex">
                 {SocialMediaList()}
             </NavigationMenuList>
@@ -53,6 +107,18 @@ function SocialMediaList(){
                 <NavigationMenuLink href={item.href}>
                     {item.icon}
                 </NavigationMenuLink>
+            </NavigationMenuItem>
+        )
+    )
+}
+
+function footerLinksList(){
+    return(
+        footerLinks.map(item =>
+            <NavigationMenuItem key={item.key} className="text-sm hover:text-orange-300">
+                <Link to={item.path}>
+                    {item.text}
+                </Link>
             </NavigationMenuItem>
         )
     )
