5deabb8fc28f72340d5e2ddf362403fdf63d5e96
Kenneth <audielkenneth23@gmail.com>
2026-05-20 15:53:15 +0700

    filtering film based on rating

--- cinemascope/src/components/cardimagehorizontal.tsx
+++ cinemascope/src/components/cardimagehorizontal.tsx
@@ -1,5 +1,5 @@
 import {Clock, Image} from "lucide-react";
-import { Card, CardAction, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
 import { Badge } from "./ui/badge";
 import { FaStar } from "react-icons/fa";
 
@@ -16,23 +16,27 @@ export default function CardImageHorizontal({title, synopsis, year, rating, dura
     return(
         <Card className="flex flex-row gap-2 items-center">
             <div className="h-full w-40 flex items-center justify-center"><Image /></div>
-            <CardHeader className="w-120">
-                <CardAction>
-                    <Badge variant="secondary" className={`${badge === "Premium" && "text-yellow-400"}`}>{badge}</Badge>
-                </CardAction>
-                <CardTitle>{title} <span className="text-muted-foreground ml-2">{`(${year})`}</span></CardTitle>
-                <CardDescription className="mt-1">{synopsis}</CardDescription>
-                <CardDescription className="flex items-center gap-7 mt-3">
-                    <div className="flex items-center gap-1">
-                        <Clock size={16} color="#e26918"/>
-                        {`${duration}m`}
-                    </div>
-                    <div className="flex items-center gap-1">
-                        <FaStar color="#f5c518"/>
-                        {rating}
-                    </div>
-                </CardDescription>
-            </CardHeader>
+            <div className="w-140">
+                <CardHeader className="">
+                    <CardAction>
+                        <Badge variant="secondary" className={`${badge === "Premium" && "text-yellow-400"}`}>{badge}</Badge>
+                    </CardAction>
+                    <CardTitle>{title} <span className="text-muted-foreground ml-2">{`(${year})`}</span></CardTitle>
+                </CardHeader>
+                <CardContent className="w-100">
+                    <CardDescription className="mt-1">{synopsis}</CardDescription>
+                    <CardDescription className="flex items-center gap-7 mt-3">
+                        <div className="flex items-center gap-1">
+                            <Clock size={16} color="#e26918"/>
+                            {`${duration}m`}
+                        </div>
+                        <div className="flex items-center gap-1">
+                            <FaStar color="#f5c518"/>
+                            {rating}
+                        </div>
+                    </CardDescription>
+                </CardContent>
+            </div>
         </Card>
     )
 }
--- cinemascope/src/pages/filmspage.tsx
+++ cinemascope/src/pages/filmspage.tsx
@@ -4,12 +4,24 @@ import { films } from "@/data/films";
 export default function FilmsPage(){
     
     return(
-        <ul>
-            {films.map(film =>
-                <li>
-                    <CardImageHorizontal title={film.title} synopsis={film.synopsis} year={film.year} rating={film.rating} duration={film.duration} badge={film.badge}/>
-                </li>
-            )}
-        </ul>
+        <section>
+
+            <ul className="flex flex-col gap-3">
+                {ListFilm()}
+            </ul>
+        </section>
+    )
+}
+
+function ListFilm(){
+    const sortFilm = films.sort((a,b) => b.rating - a.rating)
+    const sliceFilm = sortFilm.slice(0, 5)
+
+    return(
+        sliceFilm.map(film =>
+            <li className="w-180">
+                <CardImageHorizontal title={film.title} synopsis={film.synopsis} year={film.year} rating={film.rating} duration={film.duration} badge={film.badge}/>
+            </li>
+        )
     )
 }
