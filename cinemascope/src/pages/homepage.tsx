4740d6785e5c64db40c9df8384d89dbeb3d1a676
Kenneth <audielkenneth23@gmail.com>
2026-05-21 20:54:52 +0700

    fix film card image list in homepage

--- cinemascope/src/pages/homepage.tsx
+++ cinemascope/src/pages/homepage.tsx
@@ -10,13 +10,21 @@ export default function HomePage(){
             <section className="">
                 <h2 className="mb-5">Popular Movies</h2>   
                 <ul className="grid grid-cols-4 gap-5">
-                    {films.map(film => 
-                        <li key={film.id}>
-                            <CardImage id={film.id} synopsis={film.synopsis} title={film.title} badge={film.badge} rating={film.rating} duration={film.duration} year={film.year} path={`/films/${film.id}`} />
-                        </li>
-                    )}
+                    {FilmCardImageList()}
                 </ul>
             </section>
         </>
+    )
+}
+
+function FilmCardImageList(){
+    const sampleFilms = films.slice(0,8)
+
+    return(
+        sampleFilms.map(film => 
+            <li key={film.id}>
+                <CardImage id={film.id} synopsis={film.synopsis} title={film.title} badge={film.badge} rating={film.rating} duration={film.duration} year={film.year} path={`/films/${film.id}`} />
+            </li>
+        )
     )
 }
