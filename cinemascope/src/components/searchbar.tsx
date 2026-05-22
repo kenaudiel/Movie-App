34cc31dc3d4e22be39daae055f88d24f519ba299
Kenneth <audielkenneth23@gmail.com>
2026-05-22 14:52:03 +0700

    add searchbar state & onChange handler

--- cinemascope/src/components/searchbar.tsx
+++ cinemascope/src/components/searchbar.tsx
@@ -1,11 +1,18 @@
+import {useState} from 'react'
 import { Button } from "@/components/ui/button"
 import { Field } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
 
+const [query, setQuery] = useState<string>("")
+
+function handleChange(e:React.ChangeEvent<HTMLInputElement>){
+  setQuery(e.target.value)
+}
+  
 export default function SearchBar() {
   return (
     <Field orientation="horizontal">
-      <Input className="w-150" type="search" placeholder="Search..." />
+      <Input className="w-150" type="search" placeholder="Search..." value={query} onChange={handleChange}/>
       <Button>Search</Button>
     </Field>
   )
