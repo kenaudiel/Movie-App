891e926e858a84cebf3994cefa92ce228d9591f8
Kenneth <audielkenneth23@gmail.com>
2026-05-22 11:00:49 +0700

    add duration & rating information

--- cinemascope/src/pages/filmspage.tsx
+++ cinemascope/src/pages/filmspage.tsx
@@ -1,6 +1,8 @@
 import CardImageHorizontal from "@/components/cardimagehorizontal";
 import FilmImage from "@/components/filmimage";
 import { films } from "@/data/films";
+import { Clock } from "lucide-react";
+import { FaStar } from "react-icons/fa";
 
 const sortFilm = films.sort((a,b) => b.rating - a.rating)
 const sliceFilm = sortFilm.slice(0, 5)
@@ -18,7 +20,7 @@ export default function FilmsPage(){
             </div>
             <div>
                 <p className="text-sm text-muted-foreground mb-5">MORE FILMS FOR YOU</p>
-                <ul className="grid grid-cols-3 gap-2">
+                <ul className="grid grid-cols-3 gap-x-3 gap-y-7">
                     {ListMoreFilm()}
                 </ul>
             </div>
@@ -41,7 +43,19 @@ function ListMoreFilm(){
     return(
         sliceMoreFilm.map(film =>
             <li key={film.id} className="max-w-fit">
-                <FilmImage className="w-25 h-30" path={`/films/${film.id}`}/> 
+                <div className="flex flex-col items-center gap-2">
+                    <FilmImage className="w-25 h-30 bg-card" path={`/films/${film.id}`}/>
+                    <div className="flex gap-2">
+                        <div className="flex items-center gap-1 text-xs">
+                            <Clock size={12} color="#e26918"/>
+                            {`${film.duration}m`}
+                        </div>
+                        <div className="flex items-center gap-1 text-xs">
+                            <FaStar size={12} color="#f5c518"/>
+                            {film.rating}
+                        </div>
+                    </div>
+                </div>
             </li>
         )
     )
