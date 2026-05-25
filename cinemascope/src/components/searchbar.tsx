a044ce8af37cd1e50d77127d82e74cf8571de526
Kenneth <audielkenneth23@gmail.com>
2026-05-25 10:26:50 +0700

    fix usestate hook in searchbar

--- cinemascope/src/components/searchbar.tsx
+++ cinemascope/src/components/searchbar.tsx
@@ -2,17 +2,13 @@ import {useState} from 'react'
 import { Button } from "@/components/ui/button"
 import { Field } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
-
-const [query, setQuery] = useState<string>("")
-
-function handleChange(e:React.ChangeEvent<HTMLInputElement>){
-  setQuery(e.target.value)
-}
   
 export default function SearchBar() {
+  const [query, setQuery] = useState<string>("")
+
   return (
     <Field orientation="horizontal">
-      <Input className="w-150" type="search" placeholder="Search..." value={query} onChange={handleChange}/>
+      <Input className="w-150" type="search" placeholder="Search..." value={query} onChange={e => setQuery(e.target.value)}/>
       <Button>Search</Button>
     </Field>
   )
