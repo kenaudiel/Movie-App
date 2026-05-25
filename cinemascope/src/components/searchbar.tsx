c6f79046ec68075d128a91661e7cb770e3468d25
Kenneth <audielkenneth23@gmail.com>
2026-05-25 11:26:21 +0700

    add submit handler

--- cinemascope/src/components/searchbar.tsx
+++ cinemascope/src/components/searchbar.tsx
@@ -2,14 +2,28 @@ import {useState} from 'react'
 import { Button } from "@/components/ui/button"
 import { Field } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import { useNavigate } from 'react-router-dom'
   
 export default function SearchBar() {
   const [query, setQuery] = useState<string>("")
+  const navigate = useNavigate();
+
+  function handleSubmit(e:React.SubmitEvent<HTMLFormElement>){
+    e.preventDefault();
+
+    if(query.trim() === ""){
+      return
+    }
+
+    navigate(`/search/${query}`)
+  }
 
   return (
-    <Field orientation="horizontal">
-      <Input className="w-150" type="search" placeholder="Search..." value={query} onChange={e => setQuery(e.target.value)}/>
-      <Button>Search</Button>
-    </Field>
+    <form onSubmit={handleSubmit}>
+      <Field orientation="horizontal">
+        <Input className="w-150" type="search" placeholder="Search..." value={query} onChange={e => setQuery(e.target.value)}/>
+        <Button type="submit">Search</Button>
+      </Field>
+    </form>
   )
 }
