6e08350ac75ff0f57faaab5510f85f0f0971c788
Kenneth <audielkenneth23@gmail.com>
2026-05-29 08:25:08 +0700

    add blog page header & subheader

--- cinemascope/src/pages/blogpage.tsx
+++ cinemascope/src/pages/blogpage.tsx
@@ -1,7 +1,10 @@
 export default function BlogPage(){
     return(
         <section className="min-h-114">
-            <p>Currently Under Construction</p>
+            <div className="flex flex-col gap-2">
+                <h1 className="text-3xl font-bold">Blog</h1>
+                <p className="text-muted-foreground text-lg" >Discover stories, ideas, and perspectives worth reading.</p>
+            </div>
         </section>
     )
 }
