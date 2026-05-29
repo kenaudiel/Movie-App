5e0af4812d7d5fa39f80c77d4a5a117a19257c2a
Kenneth <audielkenneth23@gmail.com>
2026-05-29 12:59:41 +0700

    add hero section

--- cinemascope/src/pages/homepage.tsx
+++ cinemascope/src/pages/homepage.tsx
@@ -1,14 +1,25 @@
 import { CardImage } from "@/components/cardimage"
+import { Button } from "@/components/ui/button"
 import { films } from "@/data/films"
 
 export default function HomePage(){
     return(
         <>
             <section className="">
+                <div className="flex flex-col gap-3 items-center justify-center mt-10">
+                    <h1 className="text-4xl font-bold">Everything Worth Your Time</h1>
+                    <p className="text-lg">
+                        A place to explore films, movies, and blogs that actually matter.
+                    </p>
+                    <div className="flex gap-5 mt-5">
+                        <Button variant="default">Get Started</Button>
+                        <Button variant="secondary">Watch Films</Button>
+                    </div>
+                </div>
                 
             </section>
             <section className="">
-                <h2 className="mb-5">Popular Movies</h2>   
+                <h2 className="mb-5 text-muted-foreground text-sm">TOP RATING FILMS</h2>   
                 <ul className="grid grid-cols-4 gap-5">
                     {FilmCardImageList()}
                 </ul>
