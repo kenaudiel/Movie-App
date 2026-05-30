64fae96f1f6194d28120e9666cb7ce4e489c20c7
Kenneth <audielkenneth23@gmail.com>
2026-05-30 16:39:43 +0700

    add topic badge in blog card

--- cinemascope/src/components/blogcard.tsx
+++ cinemascope/src/components/blogcard.tsx
@@ -1,17 +1,21 @@
 import type { blogsType } from "@/data/blogs";
 import FilmImage from "./filmimage";
-import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Card, CardAction, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Badge } from "./ui/badge";
 
 type blogCardProps = blogsType
 
-export default function BlogCard({author, date, title, description}:blogCardProps){
+export default function BlogCard({author, date, title, description, topic}:blogCardProps){
     return(
         <Card className="flex p-0">
             <FilmImage className="w-full h-50"/>
             <CardHeader className="">
-                <CardTitle className="flex flex-col w-30 justify-start text-xs text-muted-foreground mb-1 font-bold">
+                <CardTitle className="flex flex-col w-50 justify-start text-xs text-muted-foreground mb-1 font-bold ">
                     {author} | {date}
                 </CardTitle>
+                <CardAction>
+                    <Badge variant="secondary">{topic}</Badge>
+                </CardAction>
                 <CardTitle className="hover:text-orange-300 cursor-pointer">
                     {title}
                 </CardTitle>
