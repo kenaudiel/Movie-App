983ab3ace089c608d29dd110ef974645e015c6cc
Kenneth <audielkenneth23@gmail.com>
2026-05-30 16:23:33 +0700

    add blogs data

---
+++ cinemascope/src/data/blogs.ts
@@ -0,0 +1,35 @@
+type blogsType = {
+    key:number,
+    author:string,
+    date:string,
+    title:string,
+    topic:string,
+    description:string
+}
+
+const blogs:blogsType[] = [
+    {
+        key:1,
+        author:"Kenneth Audiel",
+        date:"30 May 2026",
+        title:"Top 10 Highest Rating Films in 2026",
+        topic:"Recommendation",
+        description:"Have you wonder which film should you watch tonight? We got some suggestion for you."
+    },
+    {
+        key:2,
+        author:"Lenon Jame",
+        date:"30 May 2026",
+        title:"The Avengers: Why Is It One of The Greatest Film of All Time",
+        topic:"Analysis",
+        description:"Here are some reason why The Avengers is arguably one of the best film among all."
+    },
+    {
+        key:3,
+        author:"Brian Zeke",
+        date:"31 May 2026",
+        title:"All Spider-Man Movies Ranked",
+        topic:"Ranking",
+        description:"As a spiderman fan, here is how I would rank every spider-man movie."
+    }
+]
