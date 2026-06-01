762445299029ca9840afec482132b203c44c9616
Kenneth <audielkenneth23@gmail.com>
2026-06-01 15:06:18 +0700

    add famous characters quotes

---
+++ cinemascope/src/data/quotes.ts
@@ -0,0 +1,33 @@
+type quotesTypes = {
+    name:string,
+    film:string,
+    quote:string
+}
+
+export const quotes:quotesTypes[] = [
+    {
+        name:"Albus Dumbledore",
+        film:"Harry Potter",
+        quote:"It takes a great deal of bravery to stand up to your enemies, but a great deal more to stand up to your friends."
+    },
+    {
+        name:"The Joker",
+        film:"The Dark Knight",
+        quote:"If you're good at something, never do it for free."
+    },
+    {
+        name:"BoJack Horseman",
+        film:"BoJack Horseman",
+        quote:"One day, you're going to look around and you're going to realize that everybody loves you, but nobody likes you. And that is the loneliest feeling in the world."
+    },
+    {
+        name:"Maximus",
+        film:"Gladiator",
+        quote:"One day, you're going to look around and you're going to realize that everybody loves you, but nobody likes you. And that is the loneliest feeling in the world.Death smiles at us all. All a man can do is smile back."
+    },
+    {
+        name:"The Doctor",
+        film:"Doctor Who",
+        quote:"We're all stories in the end, we just have to make it a good one."
+    },
+]
--- cinemascope/src/pages/blogpage.tsx
+++ cinemascope/src/pages/blogpage.tsx
@@ -1,7 +1,5 @@
 import BlogCard from "@/components/blogcard";
 import CarouselCard from "@/components/carouselcard";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import { blogs } from "@/data/blogs";
 
