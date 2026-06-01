207631685f7d1456175520e59710f50838a92ba1
Kenneth <audielkenneth23@gmail.com>
2026-06-01 17:26:23 +0700

    add carousel card props

--- cinemascope/src/components/carouselcard.tsx
+++ cinemascope/src/components/carouselcard.tsx
@@ -1,15 +1,16 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Card, CardContent, CardDescription } from "./ui/card";
 import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa6";
+import type { quotesTypes } from "@/data/quotes";
 
-export default function CarouselCard(){
+export default function CarouselCard({name, film, quote}:quotesTypes){
     return(
         <Card className="w-100 px-7 py-5">
             <FaQuoteLeft color="#FFB86A" className=""/>
             <CardContent className="p-0 flex flex-col gap-3">
                 <CardDescription className="text-primary">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium nisi, totam amet unde soluta vitae, odit aperiam quos enim illum asperiores quisquam, neque accusamus ducimus! Natus repellendus quam sapiente error.
+                    {quote}
                 </CardDescription>
-                <span className="text-xs text-muted-foreground">— The Doctor, Doctor Who</span>
+                <span className="text-xs text-muted-foreground">— {name}, {film}</span>
             </CardContent>
             <FaQuoteRight color="#FFB86A" className="ml-auto"/>
         </Card>
--- cinemascope/src/data/quotes.ts
+++ cinemascope/src/data/quotes.ts
@@ -1,4 +1,5 @@
-type quotesTypes = {
+export type quotesTypes = {
+    id:number,
     name:string,
     film:string,
     quote:string
@@ -6,26 +7,31 @@ type quotesTypes = {
 
 export const quotes:quotesTypes[] = [
     {
+        id:1,
         name:"Albus Dumbledore",
         film:"Harry Potter",
         quote:"It takes a great deal of bravery to stand up to your enemies, but a great deal more to stand up to your friends."
     },
     {
+        id:2,
         name:"The Joker",
         film:"The Dark Knight",
         quote:"If you're good at something, never do it for free."
     },
     {
+        id:3,
         name:"BoJack Horseman",
         film:"BoJack Horseman",
         quote:"One day, you're going to look around and you're going to realize that everybody loves you, but nobody likes you. And that is the loneliest feeling in the world."
     },
     {
+        id:4,
         name:"Maximus",
         film:"Gladiator",
         quote:"One day, you're going to look around and you're going to realize that everybody loves you, but nobody likes you. And that is the loneliest feeling in the world.Death smiles at us all. All a man can do is smile back."
     },
     {
+        id:5,
         name:"The Doctor",
         film:"Doctor Who",
         quote:"We're all stories in the end, we just have to make it a good one."
