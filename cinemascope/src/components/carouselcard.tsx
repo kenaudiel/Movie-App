5e6b4463eb6e44413a9452434a251ff08319961b
Kenneth <audielkenneth23@gmail.com>
2026-06-01 15:27:06 +0700

    changing carousel card content & design

--- cinemascope/src/components/carouselcard.tsx
+++ cinemascope/src/components/carouselcard.tsx
@@ -1,23 +1,17 @@
-import { Avatar, AvatarFallback } from "./ui/avatar";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa6";
 
 export default function CarouselCard(){
     return(
-        <Card className="w-100">
-            <div className="flex flex-col items-center">
-                <Avatar size="lg" className="">
-                    <AvatarFallback>K</AvatarFallback>
-                </Avatar>
-            </div>
-            <CardHeader className="justify-center">
-                <CardTitle className="flex flex-row">Kenneth Audiel Manuelli</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <CardDescription className="flex justify-center">
+        <Card className="w-100 px-7 py-5">
+            <FaQuoteLeft color="#FFB86A" className=""/>
+            <CardContent className="p-0 flex flex-col gap-3">
+                <CardDescription className="text-primary">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium nisi, totam amet unde soluta vitae, odit aperiam quos enim illum asperiores quisquam, neque accusamus ducimus! Natus repellendus quam sapiente error.
                 </CardDescription>
+                <span className="text-xs text-muted-foreground">— The Doctor, Doctor Who</span>
             </CardContent>
-            
+            <FaQuoteRight color="#FFB86A" className="ml-auto"/>
         </Card>
     )
 }
